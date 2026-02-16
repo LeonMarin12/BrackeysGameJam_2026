5a4d4675f407a7099345041469b1ca90f6279ef7
León Igancio Marín Font <leonmarinfont@gmail.com>
2026-02-16 20:32:54 -0300

    changed palette

--- Main/Player/weapon_manager.gd
+++ Main/Player/weapon_manager.gd
@@ -7,7 +7,7 @@ extends Node2D
 @export_category('Trigger Settings')
 @export_range(0, 1) var shake_camera_force :float = 0.2
 @export var shake_camera_decay :float = 0.8
-@export var flash_duration :float = 0.5
+@export var flash_duration :float = 0.2
 @export var recoil_force :float = 500
 
 var mouse_direction :Vector2
